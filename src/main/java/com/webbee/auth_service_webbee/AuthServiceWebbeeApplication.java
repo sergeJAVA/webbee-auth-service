c2aaf60b1bf0d22fb18f9990358eb9a0e5f3860d
Sergey Kurochkin <serega.curo4kin2014@gmail.com>
2025-07-11 10:41:06 +0300

    first commit

---
+++ src/main/java/com/webbee/auth_service_webbee/AuthServiceWebbeeApplication.java
@@ -0,0 +1,15 @@
+package com.webbee.auth_service_webbee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceWebbeeApplication {
+
+public static void main(String[] args) {
+
+SpringApplication.run(AuthServiceWebbeeApplication.class, args);
+
+}
+
+}
